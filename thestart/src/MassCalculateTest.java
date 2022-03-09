5377021a9d25d670ee8e735650ad8880740085a9
MomoCyann <541525127@qq.com>
2022-03-09 18:09:09 +0800

    3.9

---
+++ thestart/src/MassCalculateTest.java
@@ -0,0 +1,35 @@
+public class MassCalculateTest {
+    public static void main(String[] args) {
+        //获取两个整数较大值
+        int m = 10;
+        int n = 10;
+        int max = (m > n)? m : n;
+        System.out.println(max);
+
+        String maxStr = (m > n)? "M大" : "n大";
+        System.out.println(maxStr);
+
+        String maxStr2 = (m > n)? "M大" : ((m == n)? "m等于n" : "n大");
+        System.out.println(maxStr2);
+
+        //获取三个数的最大值
+        int a = 10;
+        int b = 20;
+        int c = 50;
+
+        int max1 = (a > b)? a : b;
+        int max2 = (max1 > c)? max1 : c;
+        System.out.println(max2);
+        int max3 = (((a > b)? a : b) > c)? ((a > b)? a : b) : c; //可读性太差
+
+        //三元运算符都可以改为if-else
+        if(a > b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+        //优先三元，比if-else效率更高，但是if-else可以写的更复杂，如果本身很简单就不推荐if-else
+
+
+    }
+}
--- thestart/src/SetValueTest.java
+++ thestart/src/SetValueTest.java
@@ -1,2 +1,26 @@
+/*
+== += -= *= /= %=
+ */
 public class SetValueTest {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        short s1 = 10;
+        s1 += 2; //不会改变数据类型
+
+        //+2的操作
+        s1 += 2;
+        //+1
+        int num = 10;
+        num++;
+
+        int i = 1;
+        i *= 0.1; //0
+        i++; //1
+
+
+        int n1 = 10;
+        n1 += (n1++) + (++n1); //n1 = n1 + (n1++) + (++n1) = 10 + 10 + 12 = 32
+
+    }
 }
