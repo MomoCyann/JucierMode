b683b418fb96d96a4c4f81fdf835b31b8d0e87c8
MomoCyann <541525127@qq.com>
2022-03-15 13:08:34 +0800

    3.15

---
+++ thestart/src/LoopTest.java
@@ -0,0 +1,11 @@
+public class LoopTest {
+    public static void main(String[] args) {
+        int num = 1;
+        for(System.out.println("a"); num <= 3; System.out.println("c"), num++){
+            System.out.println("b");
+        }
+        //结果 abcbcbc
+
+        //for(;;)   while(true)
+    }
+}
